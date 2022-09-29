c5ce6e2c94f8a84b1f83a28deb53099843408322
afhiyh <112327219+Chromium05@users.noreply.github.com>
2022-09-29 22:14:07 +0700

    Tambahin pilihan satuan
    Masih ada beberapa bug yang bakal dibenerin nanti

--- converter-script.js
+++ converter-script.js
@@ -1,58 +1,80 @@
 var lang = parseInt(prompt('Pilih Bahasa/Select Language \nIndonesia (1) \nEnglish (2)')),
 	ulang = true;
 	
-function convertCenti(a) {
-	return a / 100; // cm to m
-}
-
-function convertInch(b) {
-	return b / 2.54; //cm to inch
-}
-
-function convertMili(c) {
-	return c * 10; //cm to mm 
-}
-
-function convertMiles(d) {
-	return d / 1.609; //km to mil
-}
-
-function convertFeet(e) {
-	return e * 3.281; //m to ft
-}
-	
 switch(lang) {
 	case 1:
 		while(ulang) {
-			var choice = parseInt(prompt('Pilih Unit yang Ingin Dikonversi \ncm ke m (1) \ncm ke inci (2) \ncm ke mm (3) \nkm ke mil (4) \nm ke kaki (5)'));
+			var unit1 = prompt('Pilih Satuan yang Ingin Dikonversi/Diubah \n(1) cm | (2) mm \n(3) m | (4) inci \n(5) km | (6) mil \n(7) yard | (8) kaki'),
+				unit2 = prompt('Ubah ' + unit1 + ' ke... \n(1) cm | (2) mm \n(3) m | (4) inci \n(5) km | (6) mil \n(7) yard | (8) kaki');
 			
-			if (choice == 1) {
-				var cm1 = parseInt(prompt('Masukkan Unit cm')),
-					m1 = convertCenti(cm1);
-					
-				alert(cm1 + ' cm = ' + m1 + ' m');
-			} else if (choice == 2) {
-				var cm2 = parseInt(prompt('Masukkan Unit cm')),
-					inch = convertInch(cm2);
-					
-				alert(cm2 + ' cm = ' + Math.floor(inch)*1 + ' inci');
-			} else if (choice == 3) {
-				var cm3 = parseInt(prompt('Masukkan Unit cm')),
-					mm = convertMili(cm3);
-					
-				alert(cm3 + ' cm = ' + mm + ' mm');
-			} else if (choice == 4) {
-				var km = parseInt(prompt('Masukkan Unit km')),
-					mil = convertMiles(km);
-					
-				alert(km + ' km = ' + Math.floor(mil)*1 + ' mil');
-			} else if (choice == 5) {
-				var m2 = parseInt(prompt('Masukkan Unit m')),
-					ft = convertFeet(m2);
-					
-				alert(m2 + ' m = ' + Math.floor(ft)*1 + ' kaki');
+			if (unit1 == 'cm' && unit2 == 'cm' || unit1 == 'mm' && unit2 == 'mm' || unit1 == 'm' && unit2 == 'm' || unit1 == 'inci' && unit2 == 'inci' || unit1 == 'km' && unit2 == 'km' || unit1 == 'mil' && unit2 == 'mil' || unit1 == 'yard' && unit2 == 'yard' || unit1 == 'kaki' && unit2 == 'kaki') {
+				alert('Satuannya tidak boleh sama!');
+			} else if (unit1 == 'cm' && unit2 == 'm') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' cm = ' + a/100 + ' m');
+			} else if (unit1 == 'm' && unit2 == 'cm') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' m = ' + a*100 + ' cm');
+			} else if (unit1 == 'cm' && unit2 == 'mm') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' cm = ' + a*10 + ' mm');
+			} else if (unit1 == 'mm' && unit2 == 'cm') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' mm = ' + a/10 + ' cm');
+			} else if (unit1 == 'm' && unit2 == 'mm') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' m = ' + a*1000 + ' mm');
+			} else if (unit1 == 'mm' && unit2 == 'm') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' mm = ' + a/1000 + ' m');
+			} else if (unit1 == 'cm' && unit2 == 'inci') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' cm = ' + Math.floor(a/2.54)*1 + ' inci');
+			} else if (unit1 == 'inci' && unit2 == 'cm') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' inci = ' + Math.floor(a*2.54)*1 + ' cm');
+			} else if (unit1 == 'm' && unit2 == 'km') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' m = ' + Math.floor(a/1000)*1 + ' km');
+			} else if (unit1 == 'km' && unit2 == 'm') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' km = ' + Math.floor(a*1000)*1 + ' m');
+			} else if (unit1 == 'km' && unit2 == 'mil') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' km = ' + Math.floor(a/1.6)*1 + ' mil');
+			} else if (unit1 == 'mil' && unit2 == 'km') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' mil = ' + Math.floor(a*1.6)*1 + ' km');
+			}  else if (unit1 == 'm' && unit2 == 'yard') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' m = ' + Math.floor(a*1.094)*1 + ' yard');
+			} else if (unit1 == 'yard' && unit2 == 'm') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' yard = ' + Math.floor(a/1.094)*1 + ' m');
+			} else if (unit1 == 'm' && unit2 == 'kaki') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' m = ' + Math.floor(a*3.281)*1 + ' kaki');
+			} else if (unit1 == 'kaki' && unit2 == 'm') {
+				var a = parseInt(prompt('Berapa ' + unit1 + '?'));
+				
+				alert(a + ' kaki = ' + Math.floor(a/3.281)*1 + ' m');
 			} else {
-				alert('Input Salah!');
+				alert('Satuan Tidak Ada di Daftar, Mohon Coba yang Lainnya.');
 			}
 			
 			ulang = confirm('Mau hitung lagi?');
@@ -62,35 +84,77 @@ switch(lang) {
 	break;
 	case 2:
 		while(ulang) {
-			var choice = parseInt(prompt('Select the Unit You Want to Convert \ncm to m (1) \ncm to inch (2) \ncm to mm (3) \nkm to mil (4) \nm to feet (5)'));
+			var unit1 = prompt('Select the Unit You Want to Convert \n(1) cm | (2) mm \n(3) m | (4) inch \n(5) km | (6) miles \n(7) yard | (8) feet'),
+				unit2 = prompt('Convert ' + unit1 + ' to... \n(1) cm | (2) mm \n(3) m | (4) inch \n(5) km | (6) miles \n(7) yard | (8) feet');
 			
-			if (choice == 1) {
-				var cm1 = parseInt(prompt('Enter cm Unit')),
-					m1 = convertCenti(cm1);
-					
-				alert(cm1 + ' cm = ' + m1 + ' m');
-			} else if (choice == 2) {
-				var cm2 = parseInt(prompt('Enter cm Unit')),
-					inch = convertInch(cm2);
-					
-				alert(cm2 + ' cm = ' + Math.floor(inch)*1 + ' inch');
-			} else if (choice == 3) {
-				var cm3 = parseInt(prompt('Enter cm Unit')),
-					mm = convertMili(cm3);
-					
-				alert(cm3 + ' cm = ' + mm + ' mm');
-			} else if (choice == 4) {
-				var km = parseInt(prompt('Enter km Unit')),
-					mil = convertMiles(km);
-					
-				alert(km + ' km = ' + Math.floor(mil)*1 + ' mil');
-			} else if (choice == 5) {
-				var m2 = parseInt(prompt('Enter m Unit')),
-					ft = convertFeet(m2);
-					
-				alert(m2 + ' m = ' + Math.floor(ft)*1 + ' ft');
+			if (unit1 == 'cm' && unit2 == 'cm' || unit1 == 'mm' && unit2 == 'mm' || unit1 == 'm' && unit2 == 'm' || unit1 == 'inch' && unit2 == 'inch' || unit1 == 'km' && unit2 == 'km' || unit1 == 'miles' && unit2 == 'miles' || unit1 == 'yard' && unit2 == 'yard' || unit1 == 'feet' && unit2 == 'feet') {
+				alert('Cannot convert the same unit!');
+			} else if (unit1 == 'cm' && unit2 == 'm') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' cm = ' + a/100 + ' m');
+			} else if (unit1 == 'm' && unit2 == 'cm') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' m = ' + a*100 + ' cm');
+			} else if (unit1 == 'cm' && unit2 == 'mm') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' cm = ' + a*10 + ' mm');
+			} else if (unit1 == 'mm' && unit2 == 'cm') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' mm = ' + a/10 + ' cm');
+			} else if (unit1 == 'm' && unit2 == 'mm') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' m = ' + a*1000 + ' mm');
+			} else if (unit1 == 'mm' && unit2 == 'm') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' mm = ' + a/1000 + ' m');
+			} else if (unit1 == 'cm' && unit2 == 'inch') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' cm = ' + Math.floor(a/2.54)*1 + ' inch');
+			} else if (unit1 == 'inch' && unit2 == 'cm') {
+				var a = parseInt(prompt('How many ' + unit1 + 'es?'));
+				
+				alert(a + ' inch = ' + Math.floor(a*2.54)*1 + ' cm');
+			} else if (unit1 == 'm' && unit2 == 'km') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' m = ' + Math.floor(a/1000)*1 + ' km');
+			} else if (unit1 == 'km' && unit2 == 'm') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' km = ' + Math.floor(a*1000)*1 + ' m');
+			} else if (unit1 == 'km' && unit2 == 'miles') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' km = ' + Math.floor(a/1.6)*1 + ' mi');
+			} else if (unit1 == 'miles' && unit2 == 'km') {
+				var a = parseInt(prompt('How many ' + unit1 + '?'));
+				
+				alert(a + ' mi = ' + Math.floor(a*1.6)*1 + ' km');
+			}  else if (unit1 == 'm' && unit2 == 'yard') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' m = ' + Math.floor(a*1.094)*1 + ' yard');
+			} else if (unit1 == 'yard' && unit2 == 'm') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' yard = ' + Math.floor(a/1.094)*1 + ' m');
+			} else if (unit1 == 'm' && unit2 == 'feet') {
+				var a = parseInt(prompt('How many ' + unit1 + 's?'));
+				
+				alert(a + ' m = ' + Math.floor(a*3.281)*1 + ' ft');
+			} else if (unit1 == 'feet' && unit2 == 'm') {
+				var a = parseInt(prompt('How many ' + unit1 + '?'));
+				
+				alert(a + ' ft = ' + Math.floor(a/3.281)*1 + ' m');
 			} else {
-				alert('Wrong Input!');
+				alert("Unit(s) Aren't Available on the List, Please Try Something Else.");
 			}
 			
 			ulang = confirm('Wanna do it again?');
